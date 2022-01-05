c4f151fd1350f2b6f2c42f803396c67941dfae80
JADHAV SACHIN RAJHANS <srjadhav886@gmail.com>
2022-01-05 21:52:42 +0530

    first commit

--- src/1.js
+++ src/1.js
@@ -1,11 +1,48 @@
-const mysql = require("mysql2");
-const dbc = {
-    host: "localhost", // location of the database.
+const mysql = require("mysql");
+const Promise = require("bluebird");
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+const dbinfo = {
+    host: "localhost",
     user: "root",
     password: "cdac",
-    database: "miniweb",
+    database: "mini",
 };
-const connection = mysql.createConnection(dbc);
-connection.connect();
-console.log("CONNECTION SUCCESS!!!!");
-connection.end();
+async function selectuser() {
+    const connection = mysql.createConnection(dbinfo);
+    await connection.connectAsync();
+
+    let sql = `select * from ss`;
+
+    const list = await connection.queryAsync(sql, [5]);
+    console.log(list);
+    await connection.endAsync();
+    return list;
+};
+
+// async function adduser() {
+//     const connection = mysql.createConnection(dbinfo);
+//     await connection.connectAsync();
+//     let sql = `insert into ss (username,password) values(?,?)`;
+//     await connection.queryAsync(sql, [user.username, user.password]);
+//     await connection.endAsync();
+// };
+
+
+// async function checkConnection() {
+//     const connection = mysql.createConnection(dbinfo);
+//     await connection.connectAsync();
+//     console.log("Done!");
+
+//     await connection.endAsync();
+// };
+
+// const user = { username: "sachin", password: "srjadhav" };
+// const user = { username: "anita", password: "rjadhav" };
+// const user = { username: "rajhans", password: "sjadhav" };
+// const user = { username: "vaishnavi", password: "jadhav" };
+// const user = { username: "vikas", password: "srj" };
+// checkConnection();
+
+
+// adduser(user);
+selectuser();
